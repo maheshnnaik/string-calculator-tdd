2c9067533a955e52a0b1d468ff0c4ccd11486b39
maheshnnaik <maheshnaik2747@gmail.com>
2024-11-17 17:03:27 +0530

    update test case

--- test/StringCalculator.test.js
+++ test/StringCalculator.test.js
@@ -4,4 +4,17 @@ describe("StringCalculator", () => {
     test("hanadle empty string", () => {
         expect(StringCalculator.add("")).toBe(0);
     });
+
+    test("hanadle one number", () => {
+        expect(StringCalculator.add("1")).toBe(1);
+    });
+
+    test("hanadle multiple numbers separated by comma", () => {
+        expect(StringCalculator.add("1,2,3,23")).toBe(29);
+    });
+
+    test("hanadle multiple numbers separated by newline", () => {
+        expect(StringCalculator.add("1\n2,3")).toBe(6);
+    });
+
 });
