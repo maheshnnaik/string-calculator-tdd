b55e8181df06b2de5b801ccdafaf56eacfebe9ea
maheshnnaik <maheshnaik2747@gmail.com>
2024-11-17 17:04:49 +0530

    throw error for if string contains negative numbers

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ class StringCalculator {
             numbers = numberParts[1];
         }
         const nums = numbers.split(delimiters).map(Number);
+        const negatives = nums.filter(num => num < 0);
+        if (negatives.length > 0) {
+            throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
+        }
         return nums.reduce((sum, num) => sum + num, 0);
     }
 }
